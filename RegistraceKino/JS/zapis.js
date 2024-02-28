433637f16421a10fd241bf9d18cb4724f078f9d3
21ic25_hanout <21ic25_hanout@U5-19>
2024-02-28 10:19:48 +0100

    Comments v0.0.12

--- RegistraceKino/JS/zapis.js
+++ RegistraceKino/JS/zapis.js
@@ -1,5 +1,5 @@
-let nameInput = document.getElementById("nameInput");
-let emailInput = document.getElementById("emailInput");
+let nameInput = document.getElementById("nameInput"); //dostávám element s ID "nameInput"
+let emailInput = document.getElementById("emailInput"); //dostávám element s ID "nameInput" | emailInput původně sloužil pro Email, po dokončení kódu změna email na příjmení --> nebudu to měnit
 
 let poleSedadel = [
   { sedadlo: "A1", rezervace: { occupied: false, name: "", email: "" } },
@@ -56,110 +56,121 @@ let poleSedadel = [
   { sedadlo: "I4", rezervace: { occupied: false, name: "", email: "" } },
   { sedadlo: "I5", rezervace: { occupied: false, name: "", email: "" } },
   { sedadlo: "I6", rezervace: { occupied: false, name: "", email: "" } },
-];
+]; //pole pro výpis sedadel
 
-let penize = 1;
 
-function generateTableHTML() {
+function generateTableHTML() { //funkce na generování sedadel v kině
   let tableHTML = "<table class='tableHTML'";
 
   poleSedadel.forEach((x, index) => {
-    if (index % 6 === 0) {
+    if (index % 6 === 0) { //pokud je index : 6 = 0 a není 0, přídá tableRow na zarovnání dokumentu
       if (index !== 0) {
         tableHTML += "</tr>";
       }
       tableHTML += "<tr>";
     }
-    let backgroundColor = x.rezervace.occupied ? "red" : "";
+    let backgroundColor = x.rezervace.occupied ? "red" : ""; //if podmínka, která určuje pokud je sedadlo kliknté, změní barvu pozadí
     let buttonText = x.sedadlo;
     if (x.rezervace.occupied) {
-      buttonText += `<br/>${x.rezervace.name}<br/>${x.rezervace.email}`;
+      buttonText += `<br/>${x.rezervace.name}<br/>${x.rezervace.email}`; //výpis registrovaného jména a emailu na sedadlo (button)
     } else {
-      buttonText += `<br/>Volné sedadlo<br/>180Kč`;
+      let penize = 0; //proměnná peníze, která se vypisuje částku za film
+      switch(document.getElementById("selFil").value){ //switch, který mění vypsanou částku za film, podle selectu
+        case "Top Gun: Maverick - 250kč":
+          penize = 250
+          break;
+        case "Le Mans - 300kč":
+          penize = 300
+          break;
+        case "Fight Club - 180Kč":
+          penize = 180
+          break;
+      }
+      buttonText += `<br/>Volné sedadlo<br/>${penize}Kč`; //pokud není registrované, je v základu
     }
 
-    tableHTML += `<td><button class="reserveBtn btn my-2" id="id${index}" style="background-color: ${backgroundColor};" onClick="reserve(${index})">${buttonText}</button></td>`;
+    tableHTML += `<td><button class="reserveBtn btn my-2" id="id${index}" style="background-color: ${backgroundColor};" onClick="reserve(${index})">${buttonText}</button></td>`; //výpis jednotlivých buttonu z pole pomocí forEach
   });
-  tableHTML += `</tr></table><br/><p class="lead">`;
+  tableHTML += `</tr></table>`;
   return tableHTML;
 }
 
 function reserve(index) {
-  const seat = poleSedadel[index];
-  if (!seat.rezervace.occupied) {
+  const seat = poleSedadel[index]; //proměnná seat, která bere jednotlivé prvky poleSedadel pomocí indexu
+  if (!seat.rezervace.occupied) { //pokud seat.rezervace.occupied není v základním tvaru (Základní tvar --> (false,"",""))
     const name = nameInput.value.trim();
     const email = emailInput.value.trim();
     if (name != "" && email != "") {
-      seat.rezervace.occupied = true;
-      seat.rezervace.name = name;
-      seat.rezervace.email = email;
-      root.innerHTML = generateTableHTML();
+      seat.rezervace.occupied = true; //změní false na true --> obsazeno
+      seat.rezervace.name = name; //doplní jméno do rezervace v poli
+      seat.rezervace.email = email; //doplní email do rezervace v poli
+      root.innerHTML = generateTableHTML(); //vygeneruje znovu pole, ale aktualizované
     } else {
-      alert("Vyplňte prosím všechna povinná pole!");
+      alert("Vyplňte prosím všechna povinná pole!"); //alert
     }
   } else {
-    const name = nameInput.value.trim();
-    const email = emailInput.value.trim();
-    if (nameInput.value.trim() == seat.rezervace.name && emailInput.value.trim() == seat.rezervace.email) {
-      seat.rezervace.occupied = false;
-      seat.rezervace.name = "";
-      seat.rezervace.email = "";
+    const name = nameInput.value.trim(); //proměnná pro osekaný nameInput
+    const email = emailInput.value.trim(); //proměnná pro osekaný emailInput
+    if (name == seat.rezervace.name && email == seat.rezervace.email) { //kontroluje, zdali je zadaný email stejný, jako uložený email. Poté můžete zrušit rezervaci
+      seat.rezervace.occupied = false; //vrácení rezervovaného (true) sedadla na nerezervované (false)
+      seat.rezervace.name = ""; //Vymazání name v rezervaci
+      seat.rezervace.email = ""; //Vymazání email v rezervaci
     }else{
-        alert("Toto sedadlo je již obsazené!");
+        alert("Toto sedadlo je již obsazené!"); //alert
     }
 
-    root.innerHTML = generateTableHTML();
+    root.innerHTML = generateTableHTML(); //Vygeneruje znovu aktualizovaný table
   }
   
 }
 
-document.getElementById("resBtn").onclick = () => {
-  saveToLocalStorage(poleSedadel);
-  root.innerHTML = generateTableHTML();
+document.getElementById("resBtn").onclick = () => { //funkce on click na buttonu s ID "resBtn" --> reserve button
+  saveToLocalStorage(poleSedadel); //uloží data, do localStorage - uloží stav ve kterém je poleSedadel
+  root.innerHTML = generateTableHTML(); //aktualizuje pole
 };
 
-function saveToLocalStorage(data) {
-  localStorage.setItem(
-    document.getElementById("selFil").value,
-    JSON.stringify(data)
+function saveToLocalStorage(data) { //funkce na ukládání dat (data = data, které bychom chtěli uložit --> poleSedadel)
+  localStorage.setItem( //setItem ukládá do localStorage
+    document.getElementById("selFil").value, //klíč k uložení, je název filmu
+    JSON.stringify(data) //stringify převede hodnotu na JS Object Notation - JSON
   );
 }
 
 
-function loadFromLocalStorage() {
-  const storedData = localStorage.getItem(
-    document.getElementById("selFil").value
+function loadFromLocalStorage() { //načítání z localStorage
+  const storedData = localStorage.getItem( //getItem načítá z localStorage
+    document.getElementById("selFil").value //načítáme pomocí uloženého klíče
   );
   if (storedData) {
-    poleSedadel = JSON.parse(storedData);
+    poleSedadel = JSON.parse(storedData); //parsuje JSON --> konstruuje JS objekt
   }
 }
 
-function generateDifferentReservation() {
+function generateDifferentReservation() { //funkce pro nastavení pole do původní stavu
  
-  poleSedadel.forEach(
-    (seat) => (seat.rezervace = { occupied: false, name: "", email: "" })
+  poleSedadel.forEach( //projede celé poleSedadel a přepíše rezervace:.. na původní stav -> (false, "", "")
+    (seat) => (seat.rezervace = { occupied: false, name: "", email: "" }) //nastavení rezervace:.. na (false, "", "")
   );
-  loadFromLocalStorage();
-  root.innerHTML = generateTableHTML();
+  loadFromLocalStorage(); //volání funkce na načtení z databáze --> načte po přepsání na původní stav --> žádná kolize mezi uloženými sedadly
+  root.innerHTML = generateTableHTML(); //aktualizuje pole
 }
 
 
 
-function handleFilmSelection() {
-  selectFilm.value;
+function vyberFilm() { //funkce pro poslouchání eventu change --> onChange --> kód
+  selectFilm.value; //value selectu
 
-  generateDifferentReservation();
+  generateDifferentReservation(); //aktualizuje pole
 }
 
 
 
-let root = document.getElementById("root");
+let root = document.getElementById("root"); //div s ID root, kam načítáme poleSedadel
 
-loadFromLocalStorage();
+loadFromLocalStorage(); //načítaní z localStorage při zmáčknutí F5 (reload stránky)
 
-root.innerHTML = generateTableHTML();
+root.innerHTML = generateTableHTML(); //vypisování poleSedadel do div#root elementu
 
-let selectFilm = document.getElementById("selFil");
-selectFilm.addEventListener("change", handleFilmSelection);
+let selectFilm = document.getElementById("selFil"); //dostávám select#selFil
+selectFilm.addEventListener("change", vyberFilm); //EventListener na změnu v Selectu (vybrání jiného option)
 
